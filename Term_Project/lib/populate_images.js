b16723826052bde1f2b93a91832f6d84b78622ca
Logan Figgins <l.figgins13@gmail.com>
2017-04-15 13:56:54 -0700

    this should now work as inteded

---
+++ Term_Project/lib/populate_images.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+var path = require('path');
+var pgp = require('pg-promise')();
+
+var connection = {
+  host: "localhost",
+  port: 5432,
+  database: "csc667",
+  user: 'postgres',
+  password: "6848broken"
+};
+
+var db = pgp(connection);
+
+
+
+var files = fs.readdirSync("/home/euphoric/NodeJsApps/CSC667/term-project-spring-2017-sushiisthebest/Term_Project/public/images/PNG-cards-1.3");
+
+Promise.all(files.map(file=>{
+	var resolved_path = "/public/images/" + file;
+	db.any("INSERT INTO imagetable(path, name) VALUES($1, $2)" ,[resolved_path,file])
+	.then(()=>{
+		console.log('name:',file, 'path:',resolved_path, 'added to imagetable');
+	})
+	.catch(err =>{
+		console.log(err);
+	})
+}))
+.then(()=>{
+	console.log("done");
+});
