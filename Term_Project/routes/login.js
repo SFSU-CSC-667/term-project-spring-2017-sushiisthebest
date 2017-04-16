a5224d4c76f4e0f22bf680af5fe9fa6536d7977c
Logan Figgins <l.figgins13@gmail.com>
2017-04-15 23:09:26 -0700

    Almost finished views to test user registration. Tomorrow i will create the query files and methods for the backend

---
+++ Term_Project/routes/login.js
@@ -0,0 +1,18 @@
+var express  = require('express');
+var router = express.Router();
+var passport = require('passport'), LocalStrategy = require('passport-local');
+
+
+
+router.get('/',function(req,res,next){
+	res.render('login');
+});
+
+router.post('/',function(req, res, next){
+	var username = req.body.username;
+	var password = req.body.password;
+
+
+
+
+})
