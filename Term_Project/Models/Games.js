a259be89e5d421a1f0f28b77e18a723ed4f8c93e
Logan Figgins <l.figgins13@gmail.com>
2017-04-29 10:23:51 -0700

    code clean up

--- Term_Project/Models/Games.js
+++ Term_Project/Models/Games.js
@@ -1,15 +1,4 @@
 
-// var connection = {
-//   host: "localhost",
-//   port: 5432,
-//   database: "csc667",
-//   user: 'postgres',
-//   password: "6848broken"
-// };
-//
-// var pgp =  require('pg-promise')();
-// var db = pgp(connection);
-
 var db = require('../config/database');
 
 module.exports = {
