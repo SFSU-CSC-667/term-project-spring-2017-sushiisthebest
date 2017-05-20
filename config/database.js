6f9886881226bfea2f90942534d0281e4faef3b6
Logan Figgins <l.figgins13@gmail.com>
2017-05-20 09:51:31 -0700

    final push

--- config/database.js
+++ config/database.js
@@ -7,6 +7,16 @@ var connection = {
 };
 
 var pgp =  require('pg-promise')();
-var db = pgp(connection);
+var db = pgp(process.env.DATABASE_URL, (err, client => {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function(row) {
+            console.log(JSON.stringify(row));
+        });
+}));
 
 module.exports = db;
+
